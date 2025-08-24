7b750eb645470ec65c4f5480063204dfe9fc31f5
Elijah Aremu <mrelijaha@gmail.com>
2025-08-24 17:31:03 +0100

    update readme

--- Endpoints/TestFunction.cs
+++ Endpoints/TestFunction.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Project.Function.Helpers;
 
 namespace Project.Function
 {
@@ -20,9 +21,15 @@ namespace Project.Function
         {
             _logger.LogInformation("Test function processed a request (isolated).");
 
-            var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
-            await response.WriteStringAsync("Hello, this is a test function (isolated, .NET 8)!");
-            return response;
+            // Create an object to return
+            var result = new
+            {
+                Message = "Hello EA, this is a test function (isolated, .NET 8)!",
+                Timestamp = DateTime.UtcNow,
+                Status = "Success"
+            };
+
+            return await ResponseHelper.OkObjectResult(req, result);
         }
     }
 }
