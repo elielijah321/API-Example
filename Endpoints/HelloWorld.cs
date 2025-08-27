fe2ad16e1abaac1b92bc8226c30cf2aa11192ee6
Elijah Aremu <mrelijaha@gmail.com>
2025-08-27 09:38:50 +0100

    add HelloWorldFunction

---
+++ Endpoints/HelloWorld.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Project.Function.Helpers;
+
+namespace Project.Function
+{
+    public class HelloWorldFunction
+    {
+        private readonly ILogger<HelloWorldFunction> _logger;
+
+        public HelloWorldFunction(ILogger<HelloWorldFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("helloWorld")]
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "test")] HttpRequestData req)
+        {
+            _logger.LogInformation("Hello World function processed a request (isolated).");
+
+            // Create an object to return
+            var result = new
+            {
+                Message = "Hello EA, this is a test function (isolated, .NET 8)!",
+                Timestamp = DateTime.UtcNow,
+                Status = "Success"
+            };
+
+            return await ResponseHelper.OkObjectResult(req, result);
+        }
+    }
+}
